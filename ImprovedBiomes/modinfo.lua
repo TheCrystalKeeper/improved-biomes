dad3fe288d2ff801d73b26e750567d5a97c8299b
Jaron Erba <jaronerba@berkeley.edu>
2025-03-06 13:45:45 -0800

    initial mod

---
+++ ImprovedBiomes/modinfo.lua
@@ -0,0 +1,19 @@
+name ="Improved Biomes"
+description = "Enhances the biomes of the constant."
+author = "CrystalKeeper7"
+
+version = "0.0.1"
+forumthread = "7"
+
+icon_atlas = "dsticon.xml"
+icon = "dsticon.tex"
+
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+dst_compatible = true
+restart_required = false
+
+client_only_mod = false
+
+reign_of_giants_compatible = true
+api_version = 10
