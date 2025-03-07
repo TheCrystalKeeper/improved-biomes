6d0468d63dae6591ff392fcca9d61353763aad7d
Martin <m22461@gmail.com>
2025-03-06 23:46:43 -0800

    + locust prefab

--- ImprovedBiomes/modmain.lua
+++ ImprovedBiomes/modmain.lua
@@ -3,3 +3,24 @@ local require = GLOBAL.require
 require "constants"
 
 print("hello world")
+
+PrefabFiles = {
+    "locust_mob",
+}
+
+local STRINGS = GLOBAL.STRINGS
+local NAMES = STRINGS.NAMES
+local CHARACTER_NAMES = {"GENERIC","WILLOW","WOLFGANG","WENDY","WX78"
+,"WICKERBOTTOM","WOODIE","WAXWELL","WATHGRITHR","WEBBER","WINONA","WARLY","WORTOX","WORMWOOD","WURT","WALTER","WANDA"}
+function BulkAddDescription(key,desc)
+    for _, charName in ipairs(CHARACTER_NAMES) do
+        local charDet = STRINGS.CHARACTERS[charName]
+        if charDet == nil then
+            print("INVALID CHARACTER NAME", charName)
+        else
+            charDet.DESCRIBE[key] = desc
+        end
+    end
+end
+NAMES.LOCUST_MOB = "Locust"
+BulkAddDescription("LOCUST_MOB","... An insolent, buzzy thing")
